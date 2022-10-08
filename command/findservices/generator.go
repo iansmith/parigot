1c28fcaa9acfdcdf68721a7567b2a537b128a650
Ian Smith <iansmith@acm.org>
2022-10-08 10:54:59 -0400

    safety

--- command/findservices/generator.go
+++ command/findservices/generator.go
@@ -1,5 +1,34 @@
 package main
 
-func generateLocator(path string) {
-	
+import (
+	"log"
+	"os"
+	"path/filepath"
+	"text/template"
+)
+
+func generateCode(result *SearchResult) {
+	tmpl := template.Must(template.ParseFS(templateFS, "template/*.tmpl"))
+	for _, project := range result.ProjectsFound {
+		for _, svc := range project.ServicesFound {
+			err := generateCodeForService(tmpl, svc, project)
+			if err != nil {
+				log.Fatalf("unable to execute template for service: %v", err)
+			}
+		}
+	}
+}
+
+func generateCodeForService(tmpl *template.Template, svc *ServiceDecl, project *ProjectDecl) error {
+	target := svc.TargetDir
+	path := filepath.Join(target, "servicedecl.go")
+	fp, err := os.Create(path)
+	if err != nil {
+		return err
+	}
+	data := map[string]interface{}{
+		"svc":     svc,
+		"project": project,
+	}
+	return tmpl.Execute(fp, data)
 }
