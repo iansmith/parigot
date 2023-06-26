2c477591a05d75308d1c5c73444dd82483f15541
Ian Smith <iansmith@parigot.info>
2023-06-26 00:18:43 +0000

    about to rewrite bar in futures

--- test/func/methodcall/impl/bar/barservice.go
+++ test/func/methodcall/impl/bar/barservice.go
@@ -6,7 +6,8 @@ import (
 
 	"github.com/iansmith/parigot/apishared/id"
 	pcontext "github.com/iansmith/parigot/context"
-	methodcall "github.com/iansmith/parigot/g/methodcall/v1"
+	"github.com/iansmith/parigot/g/methodcall/bar/v1"
+	"github.com/iansmith/parigot/g/methodcall/foo/v1"
 	syscall "github.com/iansmith/parigot/g/syscall/v1"
 	lib "github.com/iansmith/parigot/lib/go"
 )
@@ -19,12 +20,12 @@ type FullyQualifiedServiceName struct {
 
 func main() {
 	req := []lib.MustRequireFunc{
-		methodcall.MustRequire,
+		foo.MustRequire,
 	}
 	ctx := pcontext.CallTo(pcontext.SourceContext(context.Background(), pcontext.Guest), "barservice.Main")
-	bar := &barServer{}
-	binding := methodcall.InitBar(ctx, req, bar)
-	kerr := methodcall.RunBar(ctx, binding, methodcall.TimeoutInMillisBar, nil)
+	bServer := &barServer{}
+	binding := bar.Init(ctx, req, bServer)
+	kerr := bar.Run(ctx, binding, bar.TimeoutInMillis, nil)
 	if kerr != syscall.KernelErr_NoError {
 		pcontext.Fatalf(ctx, "RunBar exited with an error: %s", syscall.KernelErr_name[int32(kerr)])
 	}
@@ -32,7 +33,7 @@ func main() {
 
 // this type better implement methodcall.v1.BarService
 type barServer struct {
-	foo methodcall.Foo
+	foo foo.Client
 }
 
 //
@@ -40,31 +41,30 @@ type barServer struct {
 // defined in bar.proto.
 //
 
-func (b *barServer) Accumulate(ctx context.Context, req *methodcall.AccumulateRequest) (*methodcall.AccumulateResponse,
-	methodcall.MethodCallSuiteErr) {
-	resp := &methodcall.AccumulateResponse{}
+func (b *barServer) Accumulate(ctx context.Context, req *bar.AccumulateRequest) (*bar.AccumulateResponse, bar.BarErr) {
+	resp := &bar.AccumulateResponse{}
 	if len(req.Value) == 0 {
 		resp.Product = 0
 		resp.Sum = 0
-		return resp, methodcall.MethodCallSuiteErr_NoError
+		return resp, bar.BarErr_NoError
 	}
 
-	reqAdd := &methodcall.AddMultiplyRequest{
+	reqAdd := &foo.AddMultiplyRequest{
 		IsAdd: true,
 	}
-	reqMul := &methodcall.AddMultiplyRequest{
+	reqMul := &foo.AddMultiplyRequest{
 		IsAdd: false,
 	}
 
 	reqAdd.Value1 = 0 //identity to start
 	reqMul.Value1 = 1 // identity to start
 
-	var respAdd, respMul *methodcall.AddMultiplyResponse
+	var respAdd, respMul *foo.AddMultiplyResponse
 	for i := 0; i < len(req.GetValue()); i++ {
 		reqAdd.Value0 = req.GetValue()[i]
 		respAdd, multErr := b.foo.AddMultiply(ctx, reqAdd)
 		if int32(multErr) != 0 {
-			return nil, multErr
+			return nil, bar.BarErr_AddMultFailed
 		}
 		// b.log(nil, pblog.LogLevel_LOG_LEVEL_DEBUG, "ADD (%d,%d) iteration #%d, result add %d",
 		// 	reqAdd.GetValue0(), reqAdd.GetValue1(), i, respAdd.Result)
@@ -74,7 +74,7 @@ func (b *barServer) Accumulate(ctx context.Context, req *methodcall.AccumulateRe
 		reqMul.Value0 = req.GetValue()[i]
 		respMul, errId := b.foo.AddMultiply(ctx, reqMul)
 		if int32(errId) != 0 {
-			return nil, errId
+			return nil, bar.BarErr_AddMultFailed
 		}
 		// b.log(nil, pblog.LogLevel_LOG_LEVEL_DEBUG, "MUL (%d,%d) iteration #%d, result mul %d",
 		// 	reqMul.GetValue0(), reqMul.GetValue1(), i, respMul.Result)
@@ -82,13 +82,15 @@ func (b *barServer) Accumulate(ctx context.Context, req *methodcall.AccumulateRe
 	}
 	resp.Product = respMul.GetResult()
 	resp.Sum = respAdd.GetResult()
-	return resp, methodcall.MethodCallSuiteErr_NoError
+	return resp, bar.BarErr_NoError
 }
 
 // Ready is a check, if this returns false the library will abort and not attempt to run this service.
 // Normally this is used to do LocateXXX() calls that are needed for
 // the operation of the service.
-func (b *barServer) Ready(ctx context.Context, sid id.ServiceId) bool {
-	b.foo = methodcall.MustLocateFoo(ctx, sid)
-	return true
+func (b *barServer) Ready(ctx context.Context, sid id.ServiceId) *lib.BaseFuture[bool] {
+	b.foo = foo.MustLocate(ctx, sid)
+	f := lib.NewBaseFuture[bool]()
+	f.Set(true)
+	return f
 }
--- test/func/methodcall/impl/foo/fooservice.go
+++ test/func/methodcall/impl/foo/fooservice.go
@@ -6,10 +6,11 @@ import (
 	"unsafe"
 
 	"github.com/iansmith/parigot/apishared/id"
-	"github.com/iansmith/parigot/apiwasm"
 	pcontext "github.com/iansmith/parigot/context"
+	foo "github.com/iansmith/parigot/g/methodcall/foo/v1"
 	methodcall "github.com/iansmith/parigot/g/methodcall/v1"
 	"github.com/iansmith/parigot/g/syscall/v1"
+	lib "github.com/iansmith/parigot/lib/go"
 	"github.com/iansmith/parigot/test/func/methodcall/impl/foo/const_"
 )
 
@@ -18,16 +19,16 @@ var _ = unsafe.Sizeof([]byte{})
 const pathPrefix = "/parigotvirt/"
 
 func main() {
-	req := []apiwasm.MustRequireFunc{}
+	req := []lib.MustRequireFunc{}
 	ctx := pcontext.CallTo(pcontext.SourceContext(pcontext.NewContextWithContainer(context.Background(), "fooservice.Main"), pcontext.Guest), "fooservice.Main")
-	foo := &fooServer{}
-	binding := methodcall.InitFoo(ctx, req, foo)
+	fooServ := &fooServer{}
+	binding := foo.Init(ctx, req, fooServ)
 	var kerr syscall.KernelErr
 	for {
-		if len(binding.Pair()) == 0 {
-			panic("BINDING EMPTY")
+		if binding.Len() == 0 {
+			panic("no methods found on binding for foo")
 		}
-		kerr = methodcall.ReadOneAndCallFoo(ctx, binding, methodcall.TimeoutInMillisFoo)
+		kerr = methodcall.ReadOneAndCall(ctx, binding, foo.TimeoutInMillis)
 		if kerr == syscall.KernelErr_ReadOneTimeout {
 			pcontext.Infof(ctx, "waiting for calls to foo service")
 			continue
@@ -50,21 +51,19 @@ type fooServer struct {
 // defined in foo.proto.
 //
 
-func (f *fooServer) AddMultiply(ctx context.Context, req *methodcall.AddMultiplyRequest) (*methodcall.AddMultiplyResponse,
-	methodcall.MethodCallSuiteErr) {
-	//f.log(pctx, pblog.LogLevel_LOG_LEVEL_DEBUG, "received call for fooServer.AddMultiply")
-	resp := &methodcall.AddMultiplyResponse{}
+func (f *fooServer) AddMultiply(ctx context.Context, req *foo.AddMultiplyRequest) (*foo.AddMultiplyResponse, foo.FooErr) {
+	resp := &foo.AddMultiplyResponse{}
 	if req.IsAdd {
 		resp.Result = req.Value0 + req.Value1
 	} else {
 		resp.Result = req.Value0 * req.Value1
 	}
-	return resp, methodcall.MethodCallSuiteErr_NoError
+	return resp, foo.FooErr_NoError
 }
 
-func (f *fooServer) LucasSequence(ctx context.Context) (*methodcall.LucasSequenceResponse, methodcall.MethodCallSuiteErr) {
+func (f *fooServer) LucasSequence(ctx context.Context) (*foo.LucasSequenceResponse, foo.FooErr) {
 	pcontext.Debugf(ctx, "LucasSequence", "received call for fooServer.LucasSequence")
-	resp := &methodcall.LucasSequenceResponse{}
+	resp := &foo.LucasSequenceResponse{}
 	seq := make([]int32, const_.LucasSize) // -2 because first two are given
 	seq[0] = 2
 	seq[1] = 1
@@ -72,15 +71,15 @@ func (f *fooServer) LucasSequence(ctx context.Context) (*methodcall.LucasSequenc
 		seq[i] = seq[i-1] + seq[i-2]
 	}
 	resp.Sequence = seq
-	return resp, methodcall.MethodCallSuiteErr_NoError
+	return resp, foo.FooErr_NoError
 }
 
 // Newton-Raphson method, terms values beyond about 4 are silly
-func (f *fooServer) WritePi(ctx context.Context, req *methodcall.WritePiRequest) methodcall.MethodCallSuiteErr {
+func (f *fooServer) WritePi(ctx context.Context, req *foo.WritePiRequest) foo.FooErr {
 	pcontext.Debugf(ctx, "WritePi", "received call for fooServer.AddMultiply")
 
-	if req.GetTerms() < 1 {
-		return methodcall.MethodCallSuiteErr_BadTerms
+	if req.GetTerms() < 1 || req.GetTerms() > 4 {
+		return foo.FooErr_BadParamWritePi
 	}
 	runningTotal := 3.0 // k==0 term
 
@@ -89,12 +88,14 @@ func (f *fooServer) WritePi(ctx context.Context, req *methodcall.WritePiRequest)
 		pcontext.Debugf(ctx, "WritePi", "%f", runningTotal)
 	}
 	pcontext.Infof(ctx, "WritePi result:", "%f", runningTotal)
-	return methodcall.MethodCallSuiteErr_NoError
+	return foo.FooErr_NoError
 }
 
 // Ready is a check, if this returns false the library will abort and not attempt to run this service.
 // Normally this is used to do LocateXXX() calls that are needed for
 // the operation of the service.
-func (f *fooServer) Ready(_ context.Context, _ id.ServiceId) bool {
-	return true
+func (f *fooServer) Ready(_ context.Context, _ id.ServiceId) *lib.BaseFuture[bool] {
+	fut := lib.NewBaseFuture[bool]()
+	fut.Set(true)
+	return fut
 }
