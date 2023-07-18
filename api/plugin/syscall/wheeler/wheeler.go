3215e9fdfcfd5fb5299e13f8edc81ac63bf41cbe
Ian Smith <iansmith@parigot.info>
2023-07-18 16:32:02 -0400

    updated with new export call
    Former-commit-id: 428ee0e75b55cf52477d9896e6708bbdaeca6ff9

--- api/plugin/syscall/wheeler/wheeler.go
+++ api/plugin/syscall/wheeler/wheeler.go
@@ -1,13 +1,15 @@
-package syscall
+package wheeler
 
 import (
+	"log"
+
 	"github.com/iansmith/parigot/api/shared/id"
 	syscall "github.com/iansmith/parigot/g/syscall/v1"
 	"google.golang.org/protobuf/proto"
 )
 
 type Wheeler interface {
-	In() chan inProtoPair
+	In() chan InProtoPair
 }
 
 // the only wheeler
@@ -18,14 +20,14 @@ var _wheeler = newWheeler()
 // If the err != 0, the msg should be ignored.   If the err
 // is 0, then the msg will be non-nil.
 type OutProtoPair struct {
-	Msg *proto.Message
+	Msg proto.Message
 	Err syscall.KernelErr
 }
 
 // InProtoPair is a request and the channel to send the error
 // or response to.
 type InProtoPair struct {
-	Msg *proto.Message
+	Msg proto.Message
 	Ch  chan OutProtoPair
 }
 
@@ -37,31 +39,40 @@ type hostBinding struct {
 	host    id.HostId
 }
 
+// serviceData is the set of values that we need to be
+// able to update and check so we can know what when we are
+// ready to start a program running.
+type serviceData struct {
+	reg, exp, run bool
+}
+
 // wheeler is the type that implements system calls.  It actually
 // reads a channel and responds to the requests one by one.
 // One can think of it as a wheel in that multiple different
 // callers are all trying to get info to the wheeler and it
 // is spinning around to take each one in turn.
 type wheeler struct {
-	ch                chan InProtoPair
-	pkgToServiceName  map[string]map[string][]hostBinding
-	serviceNameToImpl map[string][]hostBinding
+	ch               chan InProtoPair
+	pkgToServiceImpl map[string]map[string][]hostBinding
+	hostToService    map[string][]id.ServiceId
+	serviceToHost    map[string]id.HostId
 }
 
 // newWheeler returns a wheeler, but this should be called
 // only during the init() function.
 func newWheeler() *wheeler {
 	w := &wheeler{
-		ch:                make(chan InProtoPair),
-		pkgToServiceName:  make(map[string]map[string][]hostBinding),
-		serviceNameToImpl: make(map[string][]hostBinding),
+		ch:               make(chan InProtoPair),
+		pkgToServiceImpl: make(map[string]map[string][]hostBinding),
+		hostToService:    make(map[string][]id.ServiceId),
+		serviceToHost:    make(map[string]id.HostId),
 	}
 	go w.Run()
 	return w
 }
 
-func (w *wheeler) In() chan InProtoPair {
-	return w.ch
+func In() chan InProtoPair {
+	return _wheeler.ch
 }
 
 // export implements the export for all the given types.
@@ -69,11 +80,63 @@ func (w *wheeler) In() chan InProtoPair {
 // that this may imply the creating of a service inside
 // the wheeler for a service that is not yet registered.
 func (w *wheeler) export(req *syscall.ExportRequest) (*syscall.ExportResponse, syscall.KernelErr) {
+	sid := id.UnmarshalServiceId(req.ServiceId)
+	hid := id.UnmarshalHostId(req.HostId)
+	w.addHost(hid, sid)
 	for _, fqn := range req.GetService() {
-
+		pkg := fqn.GetPackagePath()
+		name := fqn.GetService()
+		pkg2map, ok := w.pkgToServiceImpl[pkg]
+		if !ok {
+			pkg2map = make(map[string][]hostBinding)
+			w.pkgToServiceImpl[pkg] = pkg2map
+		}
+		allBind, ok := pkg2map[name]
+		if !ok {
+			allBind = []hostBinding{}
+			pkg2map[name] = allBind
+		}
+		allBind = append(allBind, hostBinding{
+			service: sid,
+			host:    hid,
+		})
 	}
+	return &syscall.ExportResponse{}, syscall.KernelErr_NoError
+}
+
+func (w *wheeler) addHost(hid id.HostId, sid id.ServiceId) {
+	w.serviceToHost[sid.String()] = hid
+	allSvc, ok := w.hostToService[hid.String()]
+	if !ok {
+		allSvc = []id.ServiceId{}
+		w.hostToService[hid.String()] = []id.ServiceId{}
+	}
+	allSvc = append(allSvc, sid)
+}
+
+func (w *wheeler) checkHost(hid id.HostId, allSvc []id.ServiceId) {
+	//ignored for now, should be a status check to see if the
+	//alleged services are still there
 }
 
 func (w *wheeler) Run() {
+	for {
+		in := <-w.ch
+		desc := in.Msg.ProtoReflect().Descriptor()
+		var result proto.Message
+		var err syscall.KernelErr
+		switch desc.FullName() {
+		case "syscall.v1.ExportRequest":
+			result, err = w.export((*syscall.ExportRequest)(in.Msg.(*syscall.ExportRequest)))
+		default:
+			log.Printf("ERROR! wheeler received unknown type %s", desc.FullName())
+			continue
+		}
+		outPair := OutProtoPair{
+			Msg: result,
+			Err: err,
+		}
+		in.Ch <- outPair
 
+	}
 }
--- g/methodcall/v1/suiteserver.p.go
+++ g/methodcall/v1/suiteserver.p.go
@@ -57,7 +57,7 @@ func Init(ctx context.Context,require []lib.MustRequireFunc, impl MethodCallSuit
 	}()
 
 	myId := MustRegister(ctx)
-	MustExport(ctx)
+	MustExport(ctx,myId)
 	if len(require)>0 {
 		for _, f := range require {
 			f(ctx, myId)
@@ -289,8 +289,8 @@ func MustRequire(ctx context.Context, sid id.ServiceId) {
     }
 }
 
-func MustExport(ctx context.Context) {
-    _, err:=lib.Export1("methodcall.v1","method_call_suite")
+func MustExport(ctx context.Context, sid id.ServiceId) {
+    _, err:=lib.Export1("methodcall.v1","method_call_suite",sid)
     if err!=syscall.KernelErr_NoError{
         pcontext.Fatalf(ctx, "unable to export %s.%s","methodcall.v1","method_call_suite")
         panic("not able to export methodcall.v1.method_call_suite:"+syscall.KernelErr_name[int32(err)])
