0475942253032547c2c902ac9ed6aec9d8aeef8f
Ian Smith <iansmith@parigot.info>
2023-10-08 14:17:14 +0000

    changed output format

--- command/pdep/pdep.go
+++ command/pdep/pdep.go
@@ -75,7 +75,7 @@ func Main() {
 		Platform: "linux/amd64",
 		//BuildArgs: buildArg,
 		Outputs: []types.ImageBuildOutput{types.ImageBuildOutput{
-			Type: "image",
+			Type: "docker",
 			Attrs: map[string]string{
 				"name": imageName,
 				"load": "true",
